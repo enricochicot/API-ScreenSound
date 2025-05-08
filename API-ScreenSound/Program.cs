467d6dc648f4a03d4b678c98a26e2b71a0538298
Enrico Leite <enrico.leite@pca.com.br>
2025-05-08 13:27:20 -0300

    Adicionar arquivos de projeto.

---
+++ API-ScreenSound/Program.cs
@@ -0,0 +1,13 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try //caminho feliz
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta); 
+    }
+    catch (Exception ex)
+    {
+     Console.WriteLine($"Temos um problema na API: {ex.Message}");
+    }
+        
+}
