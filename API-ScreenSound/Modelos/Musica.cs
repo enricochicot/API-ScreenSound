259bd8e6d0e9783a56d04d742326734e5eb2c63d
Enrico Leite <enrico.leite@pca.com.br>
2025-05-08 13:47:57 -0300

    test - criação de uma classe com propriedades para alguns campos da API com JsonPropertyName e desserealização da resposta.

---
+++ API-ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace API_ScreenSound.Modelos
+{
+    internal class Musica
+    {
+        [JsonPropertyName("song")]//representa um metadado especifico para o nome
+        public string? Nome {  get; set; }
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+        [JsonPropertyName("duration_ms")]
+        public int Duracao { get; set; }
+        [JsonPropertyName("genre")]
+        public string Genero { get; set; }
+
+        public void ExibirDetalhesDaMusica()
+        {
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Duração: {Duracao}");
+            Console.WriteLine($"Genêro: {Genero}");
+        }
+    }
+}
--- API-ScreenSound/Program.cs
+++ API-ScreenSound/Program.cs
@@ -1,9 +1,14 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+using API_ScreenSound.Modelos;
+
+using (HttpClient client = new HttpClient())
 {
     try //caminho feliz
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta); 
+        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
